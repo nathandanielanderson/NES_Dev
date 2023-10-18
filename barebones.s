900ddca8f133fe90f9635651adaddba655448733
Nathan Anderson <nathandanielanderson@gmail.com>
2023-10-17 19:11:10 -0500

    added bitmasks for buttons anf introduced scope

--- barebones.s
+++ barebones.s
@@ -81,6 +81,28 @@ enable_rendering:
   ldy #0
 ;////////////////\\\\\\\\\\\\\\\\
 
+
+;/////// JOYPAD LOGIC \\\\\\\\\
+
+.scope Joypad
+
+  ; Controller port adresses
+  JOYPAD = $4016
+
+  ;               0    1    0    1     0     1     0     1
+  ; Button masks: A    B   Sel  Str    Up   Dwn   Lft   Rt
+  BUTTON_A = 1 << 7   
+  BUTTON_B      = 1 << 6
+  BUTTON_SELECT      = 1 << 5
+  BUTTON_START            = 1 << 4
+  BUTTON_UP                     = 1 << 3
+  BUTTON_DOWN                         = 1 << 2
+  BUTTON_LEFT                               = 1 << 1
+  BUTTON_RIGHT                                    = 1 << 0
+  
+.endscope
+
+;////////////////\\\\\\\\\\\\\\\
 forever:
   jsr read_controller
   lda #%10000000
